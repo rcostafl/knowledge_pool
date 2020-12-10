613b95a84767adc402f09567633737f24b22f95b
Rafael Costa <rcostafl@gmail.com>
2020-12-10 17:00:33 -0300

    KnowledgeBrick model completed, including seeds

--- app/models/knowledge_brick.rb
+++ app/models/knowledge_brick.rb
@@ -1,14 +1,17 @@
 class KnowledgeBrick < ApplicationRecord
+  # validates the knowledge type available
   def self.knowledge_types
     return %w[tutorial snippet command summary other]
   end
 
   belongs_to :category
 
-  # validates :title, presence: true
-  # validates :knowledge_type, presence: true, inclusion: {
-  #   in: %w[tutorial snippet command summary other], #Knowledge.types,
-  #   message: "%{value} não é um tipo válido!"
-  # }
-  # validates :description, presence: true
+  validates :title, presence: true
+
+  validates :knowledge_type, presence: true, inclusion: {
+    in: KnowledgeBrick.knowledge_types,
+    message: "%{value} não é um tipo válido de conhecimento!"
+  }
+
+  validates :description, presence: true
 end
