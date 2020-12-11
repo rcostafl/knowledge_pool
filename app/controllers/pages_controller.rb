e27cc43ad318753709fcf8e04c5447d342087953
Rafael Costa <rcostafl@gmail.com>
2020-12-10 21:42:48 -0300

    home page started

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def home
+    # this query returns all the user's categories that has no parent
+    # No parent means that the category is a base category (a database of knowledge) from the user's point of view
+    @categories = Category.where(user: current_user, parent: nil)
   end
 end
